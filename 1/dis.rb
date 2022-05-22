4f94d456d1c0374d8751a54fdabce92a9433a7db
Meritum <meritum@rambler.ru>
2022-05-22 20:08:45 +0300

    first commit

--- 1/dis.rb
+++ 1/dis.rb
@@ -8,13 +8,13 @@ puts "Введите коэффициент c:"
 c = gets.chomp.to_i
 
 discriminant = b**2.0 - 4.0 * a * c
-square_root = discriminant**(1.0/2.0)
+square_root = Math.sqrt(discriminant)
 
 if discriminant > 0
   puts "Корень №1 = #{x1 = ((0 - b) + square_root) / 2.0 * a}"
   puts "Корень №2 = #{x2 = ((0 - b) - square_root) / 2.0 * a}"
 elsif discriminant == 0
   puts "Корень = #{x = (0 - b) / 2.0 * a}"
-else 
+else
   puts "Корней нет!"
 end
