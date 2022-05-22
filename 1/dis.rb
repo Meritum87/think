504a884528b6ced32f28f1a79196bed9c8500675
Meritum <meritum@rambler.ru>
2022-05-22 19:28:50 +0300

    first commit

---
+++ 1/dis.rb
@@ -0,0 +1,20 @@
+puts "Введите коэффициент а:"
+a = gets.chomp.to_i
+
+puts "Введите коэффициент b:"
+b = gets.chomp.to_i
+
+puts "Введите коэффициент c:"
+c = gets.chomp.to_i
+
+discriminant = b**2.0 - 4.0 * a * c
+square_root = discriminant**(1.0/2.0)
+
+if discriminant > 0
+  puts "Корень №1 = #{x1 = ((0 - b) + square_root) / 2.0 * a}"
+  puts "Корень №2 = #{x2 = ((0 - b) - square_root) / 2.0 * a}"
+elsif discriminant == 0
+  puts "Корень = #{x = (0 - b) / 2.0 * a}"
+else 
+  puts "Корней нет!"
+end
---
+++ 1/plo.rb
@@ -0,0 +1,9 @@
+puts "Значение основания?"
+fundament = gets.chomp.to_i
+
+puts "Значение высоты?"
+height = gets.chomp.to_i
+
+area = fundament * height * 0.5
+
+puts "Площадь равна: #{area}"
